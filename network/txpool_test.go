13289fba2811b22c43c4470c57670cfaf169cf5c
Peyman <hassani.peyman627@gmail.com>
2023-06-09 11:21:12 +0330

    Add tests for sorting transactions in TxPool

--- network/txpool_test.go
+++ network/txpool_test.go
@@ -1,6 +1,8 @@
 package network
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/peyzor/xchain/core"
@@ -14,14 +16,33 @@ func TestTxPool(t *testing.T) {
 
 func TestTxPoolAddTx(t *testing.T) {
 	p := NewTxPool()
-	tx := core.NewTransaction([]byte("foo"))
+	txData := []byte("foo")
+	tx := core.NewTransaction(txData)
 	assert.Nil(t, p.Add(tx))
 	assert.Equal(t, p.Len(), 1)
 
-	txx := core.NewTransaction([]byte("foo"))
+	txx := core.NewTransaction(txData)
 	assert.NotNil(t, p.Add(txx))
 	assert.Equal(t, p.Len(), 1)
 
 	p.Flush()
 	assert.Equal(t, p.Len(), 0)
+}
+
+func TestSortTransactions(t *testing.T) {
+	p := NewTxPool()
+
+	txLen := 1000
+	for i := 0; i < txLen; i++ {
+		tx := core.NewTransaction([]byte(fmt.Sprintf("foo %d", i)))
+		tx.SetFirstSeen(int64(i * rand.Intn(10000)))
+		assert.Nil(t, p.Add(tx))
+	}
+
+	assert.Equal(t, txLen, p.Len())
+
+	transactions := p.Transactions()
+	for i := 0; i < len(transactions)-1; i++ {
+		assert.True(t, transactions[i].FirstSeen() <= transactions[i+1].FirstSeen())
+	}
 }
