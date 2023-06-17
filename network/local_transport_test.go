b6a61181de0c1f655b935719fa6b5e5b7ed05f8e
Peyman <hassani.peyman627@gmail.com>
2023-06-17 11:42:07 +0330

    Add Broadcast method to Transport interface

--- network/local_transport_test.go
+++ network/local_transport_test.go
@@ -1,14 +1,15 @@
 package network
 
 import (
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestConnect(t *testing.T) {
-	tra := NewLocalTransport("A").(*LocalTransport)
-	trb := NewLocalTransport("B").(*LocalTransport)
+	tra := NewLocalTransport("A")
+	trb := NewLocalTransport("B")
 
 	tra.Connect(trb)
 	trb.Connect(tra)
@@ -27,10 +28,33 @@ func TestSendMessage(t *testing.T) {
 	assert.Nil(t, tra.SendMessage(trb.Addr(), msg))
 
 	rpc := <-trb.Consume()
-	buf := make([]byte, len(msg))
-	n, err := rpc.Payload.Read(buf)
+	// buf := make([]byte, len(msg))
+	// n, err := rpc.Payload.Read(buf)
+	b, err := ioutil.ReadAll(rpc.Payload)
 	assert.Nil(t, err)
-	assert.Equal(t, n, len(msg))
-	assert.Equal(t, buf, msg)
+	// assert.Equal(t, n, len(msg))
+	assert.Equal(t, b, msg)
 	assert.Equal(t, rpc.From, tra.Addr())
+}
+
+func TestBroadcast(t *testing.T) {
+	tra := NewLocalTransport("A")
+	trb := NewLocalTransport("B")
+	trc := NewLocalTransport("C")
+
+	tra.Connect(trb)
+	tra.Connect(trc)
+
+	msg := []byte("foo")
+	assert.Nil(t, tra.Broadcast(msg))
+
+	rpcb := <-trb.Consume()
+	b, err := ioutil.ReadAll(rpcb.Payload)
+	assert.Nil(t, err)
+	assert.Equal(t, b, msg)
+
+	rpcc := <-trc.Consume()
+	c, err := ioutil.ReadAll(rpcc.Payload)
+	assert.Nil(t, err)
+	assert.Equal(t, c, msg)
 }
